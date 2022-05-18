fb4a070584775feeb29c4489050937f590bc6097
Benjamin von Polheim <ben@vp.solutions>
2022-05-18 13:42:26 +0200

    update playground

--- lib/benvp_web/live/motion_live.ex
+++ lib/benvp_web/live/motion_live.ex
@@ -1,14 +1,36 @@
 defmodule BenvpWeb.MotionLive do
   use BenvpWeb, :live_view
 
+  require Logger
+
   alias LiveMotion.JS, as: MotionJS
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, visible: true)}
+    {:ok, assign(socket, visible: true, rotate: 0)}
+  end
+
+  def handle_event("update", _params, socket) do
+    rotate = if socket.assigns.rotate == 0, do: 360, else: 0
+    {:noreply, assign(socket, rotate: rotate)}
   end
 
   def handle_event("remove", _params, socket) do
     {:noreply, assign(socket, visible: false)}
+  end
+
+  def handle_event("animation_start", _params, socket) do
+    Logger.debug("Animation started")
+    {:noreply, socket}
+  end
+
+  def handle_event("animation_complete", _params, socket) do
+    Logger.debug("Animation completed")
+    {:noreply, socket}
+  end
+
+  def handle_event("some_push", _params, socket) do
+    Logger.debug("Random push event")
+    {:noreply, socket}
   end
 
   def render(assigns) do
@@ -19,9 +41,11 @@ defmodule BenvpWeb.MotionLive do
           <LiveMotion.motion
             id="rectangle"
             class="w-24 h-24 bg-benvp-green rounded-lg"
-            initial={[opacity: 0, y: -20]}
-            animate={[opacity: 1, y: 0]}
+            initial={[opacity: 0, y: -20, rotate: 0]}
+            animate={[opacity: 1, y: 0, rotate: @rotate]}
             exit={[opacity: 0, y: -20]}
+            on_animation_start="animation_start"
+            on_animation_complete="animation_complete"
           >
           </LiveMotion.motion>
         <% end %>
@@ -47,6 +71,14 @@ defmodule BenvpWeb.MotionLive do
       <button
         type="button"
         class="px-4 py-2 bg-slate-800 rounded"
+        phx-click={MotionJS.animate([y: [0, -20, 10, 0]], [duration: 1], to: "#rectangle")}
+      >
+        Animate Square (JS)
+      </button>
+
+      <button
+        type="button"
+        class="px-4 py-2 bg-slate-800 rounded"
         phx-click={MotionJS.hide(to: "#rectangle")}
       >
         Hide Square (JS)
@@ -56,12 +88,28 @@ defmodule BenvpWeb.MotionLive do
         Hide Square (Server)
       </button>
 
+      <button type="button" class="px-4 py-2 bg-slate-800 rounded" phx-click="update">
+        Update Square (Server)
+      </button>
+
       <button
         type="button"
         class="px-4 py-2 bg-slate-800 rounded"
         phx-click={MotionJS.show(to: "#love-div", keyframes: [opacity: 1, y: 20])}
       >
-        Show some love
+        Show some love (non motion div)
+      </button>
+
+      <button
+        type="button"
+        class="px-4 py-2 bg-slate-800 rounded"
+        phx-click={
+          MotionJS.animate([rotate: [0, 45, -45, 90, 0]], [duration: 1], to: "#rectangle")
+          |> MotionJS.show(to: "#love-div", keyframes: [opacity: 1, y: 20, rotate: 180])
+          |> Phoenix.LiveView.JS.push("some_push")
+        }
+      >
+        Composed JS actions
       </button>
 
       <LiveMotion.motion
